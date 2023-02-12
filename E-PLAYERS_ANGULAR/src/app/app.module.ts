a984806fe0112f0f308fc306552b475a7f1d903c
Renatofsant <114427740+Renatofsant@users.noreply.github.com>
2023-02-12 12:27:26 -0300

    EplayersAngular

--- E-PLAYERS_ANGULAR/src/app/app.module.ts
+++ E-PLAYERS_ANGULAR/src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { CabecalhoComponent } from './shared/cabecalho/cabecalho.component';
+import { BannerComponent } from './shared/banner/banner.component';
+import { JogosComponent } from './shared/jogos/jogos.component';
+import { OfertasComponent } from './shared/ofertas/ofertas.component';
+import { LigaComponent } from './shared/liga/liga.component';
+import { RodapeComponent } from './shared/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    CabecalhoComponent,
+    BannerComponent,
+    JogosComponent,
+    OfertasComponent,
+    LigaComponent,
+    RodapeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
